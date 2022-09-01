229d31a732eea43e1cac38b7605c0ed62c5aae35
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-09-01 19:16:25 -0300

    blik led

---
+++ sketch_aug31a_led/sketch_aug31a_led.ino
@@ -0,0 +1,15 @@
+int ledPin = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(100);
+  digitalWrite(ledPin, LOW);
+  delay(100);
+
+}
